4ec596326c7c19fd0f61b04e78a9ebe6090037fb
Parthosarothi Mukherjee <parthosarothimukherjee@gmail.com>
2018-08-14 15:22:14 +0530

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,14 +1,14 @@
 from __future__ import print_function
 from Model import *
 corpus="time_corpus"
-encoder_input, decoder_input, decoder_target = prepare_data(corpus, lexfiles, 'Config_v2')
-attrs=read_config_file('Config_v2_extended')
-stime=attrs['source_time']
-ttime=attrs['target_time']
-stoken=attrs['source_tokens']
-ttoken=attrs['target_tokens']
 
 def train(mode):
+    encoder_input, decoder_input, decoder_target = prepare_data(corpus, lexfiles, 'Config_v2')
+    attrs = read_config_file('Config_v2_extended')
+    stime = attrs['source_time']
+    ttime = attrs['target_time']
+    stoken = attrs['source_tokens']
+    ttoken = attrs['target_tokens']
     encoder_input = np.asarray(encoder_input)
     decoder_target = np.asarray(decoder_target)
     nw=BadhanauNMT(stime,ttime,stoken,ttoken,32,mode)
@@ -17,6 +17,11 @@ def train(mode):
     nw.train([encoder_input,decoder_input],decoder_target,25,8)
 
 def test(string,mode):
+    attrs = read_config_file('Config_v2_extended')
+    stime = attrs['source_time']
+    ttime = attrs['target_time']
+    stoken = attrs['source_tokens']
+    ttoken = attrs['target_tokens']
     nw = BadhanauNMT(stime, ttime, stoken, ttoken, 32, mode)
     nw.create_main()
     nw.create_translator()
@@ -24,4 +29,4 @@ def test(string,mode):
 
 mode='load'
 #train(mode)
-test("w2 m3 t1",mode)
+test("w6 t17 m0",mode)
